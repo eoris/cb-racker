755afb0fde6862eedd63af5e5c6fc989587eac00
eoris <eoris.work@gmail.com>
2015-12-04 16:53:02 +0200

    pre alpha

--- config.ru
+++ config.ru
@@ -5,4 +5,5 @@ use Rack::Session::Cookie, :key => 'rack.session',
                            :secret => 'change_me',
                            :old_secret => 'also_change_me'
 use Rack::Static, :urls => ["/stylesheets"], :root => "public"
+use Rack::Reloader
 run Racker
--- lib/racker.rb
+++ lib/racker.rb
@@ -12,9 +12,11 @@ class Racker
    
   def response
     case @request.path
-    when "/" then Rack::Response.new(render("index.html.erb"))
+    when "/"           then Rack::Response.new(render("index.html.erb"))
     when "/start_game" then start_game
-    when "/update_word" then update_word
+    when "/guessing"   then guessing
+    when "/win"        then win
+    when "/lose"       then lose
     else Rack::Response.new("Not Found", 404)
     end
   end
@@ -24,26 +26,49 @@ class Racker
     ERB.new(File.read(path)).result(binding)
   end
 
-  def update_word
+  def start_game
     Rack::Response.new do |response|
-      response.set_cookie('answer', game.guess(@request.params["answer"]).join)
+      @request.session[:game] = Codebreaker::Game.new.start
+      response.delete_cookie('answer')
+      response.delete_cookie('input')
       response.redirect("/")
     end
   end
 
-  def start_game
+  def guessing
     Rack::Response.new do |response|
-      @request.session[:game] = Codebreaker::Game.new.start
-      response.redirect("/")
+      inp = @request.params["answer"]
+      answ = game.guess(@request.params["answer"]).join
+      response.set_cookie('input', inp)
+      response.set_cookie('answer', answ)
+      if game.win?
+        response.redirect('/win')
+      elsif game.lose?
+        response.redirect('/lose')
+      else
+        response.redirect("/")
+      end
     end
+  end
+
+  def win
+    Rack::Response.new(render("win.html.erb"))
+  end
+
+  def lose
+    Rack::Response.new(render("lose.html.erb"))
   end
 
   def game
     @request.session[:game]
   end
 
+  def input
+    @request.cookies['input']
+  end
+
   def answer
-    @request.cookies['answer'] || 'Wrong input'
+    @request.cookies['answer']
   end
 
 end
