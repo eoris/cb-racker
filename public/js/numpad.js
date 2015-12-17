01a077c29bfd73ec24397e94cda78aa40f547731
eoris <eoris.work@gmail.com>
2015-12-17 11:25:57 +0200

    js ready fix

--- public/js/numpad.js
+++ public/js/numpad.js
@@ -1,21 +1,23 @@
-$('#numPad button').click(function() {
-    var value = $(this).text();
-    var input = $('#guess_input');
-    if (input.val().length < 4) {
-      input.val(input.val() + value);
-      return false;
-    }
-});
+$(function() {
+    $('#numPad button').click(function() {
+        var value = $(this).text();
+        var input = $('#guess_input');
+        if (input.val().length < 4) {
+          input.val(input.val() + value);
+          return false;
+        }
+    });
 
-$('#numClr').click(function() {
-    var value = $(this).text();
-    var input = $('#guess_input');
-    input.val('');
-    return false;
-});
+    $('#numClr').click(function() {
+        var value = $(this).text();
+        var input = $('#guess_input');
+        input.val('');
+        return false;
+    });
 
-$('#numDel').click(function() {
-    var str = $('#guess_input').val();
-    $('#guess_input').val(str.substring(0, str.length - 1));
-    return false;
+    $('#numDel').click(function() {
+        var str = $('#guess_input').val();
+        $('#guess_input').val(str.substring(0, str.length - 1));
+        return false;
+    });
 });
